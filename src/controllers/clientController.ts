0c1caed2b8da932050f98bf0ee59fe506922acfd
Christian Lee <christianlee101@gmail.com>
2025-03-09 22:51:52 -0500

    Added get clients method in controller

--- src/controllers/clientController.ts
+++ src/controllers/clientController.ts
@@ -1,6 +1,5 @@
-import { Request, Response } from 'express';
+import { Response } from 'express';
 
-import { ClientUseCase } from 'usecase/clientUseCase';
 import {
   AuthenticationError,
   AuthorizationError,
@@ -8,7 +7,8 @@ import {
   NotFoundError,
   ValidationError
 } from 'domainErrors';
-import { AuthRequest } from 'types'
+import { AuthRequest } from 'types';
+import { ClientUseCase } from 'usecase/clientUseCase';
 
 export class clientController {
   private clientUseCase: ClientUseCase;
@@ -17,21 +17,49 @@ export class clientController {
     this.clientUseCase = clientUseCase;
   };
 
+  //
+  // getClients()
+  //
+  // Grabs all users specified by role (All for admins, assigned for doulas)
+  //
+  // returns:
+  //    List of users
+  //
   async getClients(
     req: AuthRequest,
     res: Response,
   ): Promise<void> {
     try {
-      const { role } = req.user;
-
-      // Returns clients based on the role
-      let clients;
-      if (role === 'admin') {
-        clients = 
-      }
+      const { email, role } = req.user;
+      // call use case to grab all users
+      const users = await this.clientUseCase.getClients(email, role);
+      res.json(users)
     } 
-    catch (error) {
-
+    catch (getError) {
+      const error = this.handleError(getError, res);
+      res.status(error.status).json({ error: error.message})
     }
   }
+
+    // Helper method to handle errors
+    private handleError(
+      error: Error, 
+      res: Response
+    ): { status: number, message: string } {
+      console.error('Error:', error.message);
+      
+      if (error instanceof ValidationError) {
+        return { status: 400, message: error.message};
+      } else if (error instanceof ConflictError) {
+        return { status: 409, message: error.message};
+      } else if (error instanceof AuthenticationError) {
+        return { status: 401, message: error.message};
+      } else if (error instanceof NotFoundError) {
+        return { status: 404, message: error.message};
+      } else if (error instanceof AuthorizationError) {
+        return { status: 403, message: error.message};
+      } else {
+        return { status: 500, message: error.message};
+      }
+    }
 }
