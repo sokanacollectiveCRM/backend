688af8389b59470496f480df5aaa4c202af85cfb
Jerry Bony <info@techluminateacademy.com>
2025-06-05 20:47:38 -0300

    fixed imports

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,13 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import express, { Express, NextFunction, Request, Response } from 'express';
-import authRoutes from 'routes/authRoutes';
-import clientRoutes from 'routes/clientRoutes';
-import emailRoutes from 'routes/EmailRoutes';
-import quickbookRoutes from 'routes/quickbooksRoutes';
-import requestRouter from 'routes/requestRoute';
-import userRoutes from 'routes/specificUserRoutes';
+import authRoutes from './routes/authRoutes';
+import clientRoutes from './routes/clientRoutes';
 import customersRoutes from './routes/customersRoutes';
+import emailRoutes from './routes/EmailRoutes';
+import quickbookRoutes from './routes/quickbooksRoutes';
+import requestRouter from './routes/requestRoute';
+import userRoutes from './routes/specificUserRoutes';
 
 import contractRoutes from './routes/contractRoutes';
 
