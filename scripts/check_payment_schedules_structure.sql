b63c386d6ff3263a32112d7d7e9aeb4251611c83
Jerry Bony <info@techluminateacademy.com>
2025-10-02 14:37:17 -0300

    merge complete

--- scripts/check_payment_schedules_structure.sql
+++
@@ -1,5 +0,0 @@
--- Check the current structure of payment_schedules table
-SELECT column_name, data_type, character_maximum_length, is_nullable, column_default
-FROM information_schema.columns
-WHERE table_name = 'payment_schedules' AND table_schema = 'public'
-ORDER BY ordinal_position;
--- scripts/show_database.sql
+++
@@ -1,5 +0,0 @@
--- Show all tables in the database
-SELECT table_name
-FROM information_schema.tables
-WHERE table_schema = 'public'
-ORDER BY table_name;
